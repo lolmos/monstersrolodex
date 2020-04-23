07303f1bbb5ad195fadb816796617a467468f685
lolmos <lol@canicacuadrada.com>
2020-04-23 22:38:04 +0200

    Updates

---
+++ precache-manifest.a1b510d66189244aa44ace05881de46b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60af54690825ffea300e7960169e99d",
+    "url": "/lolmos/monstersrolodex/index.html"
+  },
+  {
+    "revision": "a6c39cae689c9679baf1",
+    "url": "/lolmos/monstersrolodex/static/css/main.886e4439.chunk.css"
+  },
+  {
+    "revision": "8c055d5b0864233f4e49",
+    "url": "/lolmos/monstersrolodex/static/js/2.885d0759.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/lolmos/monstersrolodex/static/js/2.885d0759.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6c39cae689c9679baf1",
+    "url": "/lolmos/monstersrolodex/static/js/main.15889fad.chunk.js"
+  },
+  {
+    "revision": "214a51a4c87d347188fb",
+    "url": "/lolmos/monstersrolodex/static/js/runtime-main.196d118c.js"
+  }
+]);
